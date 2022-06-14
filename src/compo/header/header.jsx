19098c4a0e5a293e7bb0eb6f582e271aa57b754b
akshay782 <akshayk0782@gmail.com>
2022-06-14 20:07:13 +0530

    :iphone: update: add mobile design

--- src/compo/header/header.jsx
+++ src/compo/header/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import Avatar from "boring-avatars";
 import "./header.scss";
-import searchicon from './../../assets/searchicon.svg';
-import toggleicon from './../../assets/toggle.svg';
+import searchicon from "./../../assets/searchicon.svg";
+import toggleicon from "./../../assets/toggle.svg";
 
 function Header() {
+  const [open, setOpen] = useState(false);
   return (
     <header>
       <div className="container">
@@ -16,6 +18,7 @@ function Header() {
             colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
           />
         </div>
+
         <div className="searchbox">
           <div className="icon">
             <a>
@@ -23,9 +26,14 @@ function Header() {
             </a>
           </div>
           <div className="inputbox">
-            <input className="search" type="text" placeholder="search for projects"></input>
+            <input
+              className="search"
+              type="text"
+              placeholder="search for projects"
+            ></input>
           </div>
         </div>
+
         <div className="profile">
           <Avatar
             size={40}
@@ -37,8 +45,47 @@ function Header() {
             <div className="name">Oliver Smith</div>
             <div className="position">vendor</div>
           </div>
-          <div className="toggle-btn">
-          <img src={toggleicon} alt="toggle button"/>
+          <div className="toggle-btn" onClick={() => setOpen(!open)}>
+            <img src={toggleicon} alt="toggle button" />
+          </div>
+          
+          {/* mobile sidebar */}
+          <div
+            className="mob-sidebar"
+            style={{ visibility: open ? "visible" : "hidden" }}
+          >
+            <div className="mob-container">
+
+              <div className="mob-toggle-btn" onClick={() => setOpen(!open)}>
+                <img
+                  className="mob-close-icon"
+                  src={toggleicon}
+                  alt="toggle button"
+                />
+              </div>
+
+              <div className="mob-info">
+                <Avatar
+                  size={35}
+                  name="Mahalia Jackson"
+                  variant="beam"
+                  colors={["#81657E","#3EA3AF","#F0AB3D","#C271B4","#C20D90",]}
+                />
+                <div className="mob-name">Oliver Smith</div>
+                <div className="mob-position">vendor</div>
+              </div>
+              
+              <ul className="mob-list">
+                <li className="mob-listitem">Dashboard</li>
+                <li className="mob-listitem">Projects</li>
+                <li className="mob-listitem">Calender</li>
+                <li className="mob-listitem">Messages</li>
+                <li className="mob-listitem">Notifications</li>
+                <li className="mob-listitem">Contacts</li>
+                <li className="mob-listitem">Upcoming Meetings</li>
+                <li className="mob-listitem">Recent Activities</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
