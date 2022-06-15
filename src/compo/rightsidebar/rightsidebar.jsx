bf0329201b40c7f69dc2f27346a6f8c965710d3f
akshay782 <akshayk0782@gmail.com>
2022-06-15 21:06:11 +0530

    :sparkles: add: recent activity section

--- src/compo/rightsidebar/rightsidebar.jsx
+++ src/compo/rightsidebar/rightsidebar.jsx
@@ -4,8 +4,28 @@ import threedots from "./../../assets/three-dots.svg";
 import togglebtn from "./../../assets/toggle.svg";
 import Meetingcard from "./meetingcard/meetingcard";
 import Meetingtype from "./meetingtype/meetingtype";
+import Recentactivity from "./recentactivity/recentactivity";
 
 function RightSideBar() {
+
+  const recentactivities = [
+    {
+      name: "Kevin Dukkon",
+      message: "Thank you for booking a meeting with JJ",
+    },
+    {
+      name: "Ashish Sharma",
+      message: "You received a message from Ashish",
+    },
+    {
+      name: "Ashish Sharma",
+      message: "You received a message from Ashish",
+    },
+    {
+      name: "Ashish Sharma",
+      message: "You received a message from Ashish",
+    }
+  ]
 
   const[open, setOpen] = useState(false);
 
@@ -31,7 +51,15 @@ function RightSideBar() {
             <div className="three-dot-icon">
               <img src={threedots} alts="more options button"></img>
             </div>
-          </div>
+        </div>
+        
+        {
+          recentactivities.map((activity, index)=>{
+            return(
+              <Recentactivity name={activity.name} message={activity.message} index={index}></Recentactivity>
+            )
+          })
+        }
         </div>
       </div>
     </div>
