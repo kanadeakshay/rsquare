a992f753cbc6e96b8543424ce8d1c9040231456f
akshay782 <akshayk0782@gmail.com>
2022-06-15 19:55:26 +0530

    :tada: add: meetings sections

---
+++ src/compo/rightsidebar/meetingcard/meetingcard.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import threedots from "./../../../assets/three-dots.svg";
+import plussign from "./../../../assets/plus-sign.svg";
+import Avatar from 'boring-avatars';
+import './meetingcard.scss';
+
+function Meetingcard() {
+  return (
+    <div className="meetingcard">
+        <div className="meetingcard-subheading">
+            <div className="meeting-period">30 minute call with clients</div>
+            <div className="meetingcard-three-dots">
+                <img src={threedots} alt="more options" />
+            </div>
+        </div>
+        <div className="meetingcard-heading">Project Discovery call</div>
+        <div className="meetingcard-time">
+            Due soon <span className="clock">09:20 am</span>
+        </div>
+        <div className="meetingcard-people-join">
+            <div className="meetingcard-people">
+            <Avatar
+              size={35}
+              name="Mahalia Jackson"
+              variant="beam"
+              colors={["#81657E", "#3EA3AF", "#F0AB3D", "#C271B4", "#C20D90"]}
+            />
+            </div>
+            <button className="meetingcard-invite">
+                <img src={plussign} alt="invite people" />
+                Invite
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Meetingcard
--- src/compo/rightsidebar/rightsidebar.jsx
+++ src/compo/rightsidebar/rightsidebar.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./rightsidebar.scss";
 import threedots from "./../../assets/three-dots.svg";
-
+import togglebtn from "./../../assets/toggle.svg";
+import Meetingcard from "./meetingcard/meetingcard";
+import Meetingtype from "./meetingtype/meetingtype";
 
 function RightSideBar() {
+
+  const[open, setOpen] = useState(false);
 
   return (
     <div className="right-sidebar">
       <div className="right-sidebar__container">
+        
         <div className="upcoming-meetings">
           <div className="upcoming-meetings__title">
             <div className="upcoming-meetings__title__heading">Upcoming Meetings</div>
@@ -15,10 +20,19 @@ function RightSideBar() {
               <img src={threedots} alts="more options button"></img>
             </div>
           </div>
-          <div className="todays-meetings"></div>
-          <div className="schedule-meetings"></div>
+
+          <Meetingtype type="Today's Meetings"></Meetingtype>
+          <Meetingtype type="Schedule Meetings"></Meetingtype>
         </div>
-        <div className="recent-activities"></div>
+
+        <div className="recent-activities">
+        <div className="recent-activities__title">
+            <div className="recent-activities__title__heading">Recent Activities</div>
+            <div className="three-dot-icon">
+              <img src={threedots} alts="more options button"></img>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
