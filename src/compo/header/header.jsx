aa3d9c8d477cd143ba9f27c2ce2dcef2a198a038
akshay782 <akshayk0782@gmail.com>
2022-06-16 14:48:50 +0530

    :hammer: fix: mobile sidebar

--- src/compo/header/header.jsx
+++ src/compo/header/header.jsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 import Avatar from "boring-avatars";
 import "./header.scss";
 import searchicon from "./../../assets/searchicon.svg";
+import dashboardicon from "./../../assets/dashboardicon.svg";
+import projecticon from "./../../assets/project.svg";
+import calendericon from "./../../assets/calender.svg";
+import messageicon from "./../../assets/messages.svg";
+import notificationicon from "./../../assets/notifications.svg";
+import contacticon from "./../../assets/contact-icon.svg";
 import toggleicon from "./../../assets/toggle.svg";
 import RightSideBar from "../rightsidebar/rightsidebar";
 
@@ -47,7 +53,10 @@ function Header() {
               <div className="name">Oliver Smith</div>
               <div className="position">vendor</div>
             </div>
-            <div className="toggle-btn" onClick={() => setOpen(!open)}>
+            <div
+              className={open ? "toggle-btn rotate" : "toggle-btn"}
+              onClick={() => setOpen(!open)}
+            >
               <img src={toggleicon} alt="toggle button" />
             </div>
 
@@ -57,40 +66,76 @@ function Header() {
               style={{ visibility: open ? "visible" : "hidden" }}
             >
               <div className="mob-container">
-                <div className="mob-toggle-btn" onClick={() => setOpen(!open)}>
-                  <img
-                    className="mob-close-icon"
-                    src={toggleicon}
-                    alt="toggle button"
-                  />
-                </div>
+                <div className="mob-info-toggle-btn">
+                  <div
+                    className="mob-toggle-btn"
+                    onClick={() => setOpen(!open)}
+                  >
+                    <img
+                      className="mob-close-icon"
+                      src={toggleicon}
+                      alt="toggle button"
+                    />
+                  </div>
 
-                <div className="mob-info">
-                  <Avatar
-                    size={35}
-                    name="Mahalia Jackson"
-                    variant="beam"
-                    colors={[
-                      "#81657E",
-                      "#3EA3AF",
-                      "#F0AB3D",
-                      "#C271B4",
-                      "#C20D90",
-                    ]}
-                  />
-                  <div className="mob-name">Oliver Smith</div>
-                  <div className="mob-position">vendor</div>
+                  <div className="mob-info">
+                    <Avatar
+                      size={40}
+                      name="Mahalia Jackson"
+                      variant="beam"
+                      colors={[
+                        "#81657E",
+                        "#3EA3AF",
+                        "#F0AB3D",
+                        "#C271B4",
+                        "#C20D90",
+                      ]}
+                    />
+                    <div className="mob-name">Oliver Smith</div>
+                    <div className="mob-position">vendor</div>
+                  </div>
                 </div>
 
                 <ul className="mob-list">
-                  <li className="mob-listitem">Dashboard</li>
-                  <li className="mob-listitem">Projects</li>
-                  <li className="mob-listitem">Calender</li>
-                  <li className="mob-listitem">Messages</li>
-                  <li className="mob-listitem">Notifications</li>
-                  <li className="mob-listitem">Contacts</li>
-                  <li className="mob-listitem">Upcoming Meetings</li>
-                  <li className="mob-listitem">Recent Activities</li>
+                  <li className="mob-listitem">
+                    <img src={dashboardicon} alt="icon" />
+                    <div className="mob-listitem-name">Dashboard</div>
+                  </li>
+                  <li className="mob-listitem">
+                    <img src={projecticon} alt="icon" />
+                    <div className="mob-listitem-name">Projects</div>
+                  </li>
+                  <li className="mob-listitem">
+                    <img src={calendericon} alt="icon" />
+                    <div className="mob-listitem-name">Calender</div>
+                  </li>
+                  <li className="mob-listitem">
+                    <img src={messageicon} alt="icon" />
+                    <div className="mob-listitem-name">Messages</div>
+                    <div className="counts">5</div>
+                  </li>
+                  <li className="mob-listitem">
+                    <img src={notificationicon} alt="icon" />
+                    <div className="mob-listitem-name">Notifications</div>
+                    <div className="counts">5</div>
+                  </li>
+                  <li className="mob-listitem">
+                    <img src={contacticon} alt="icon" />
+                    <div className="mob-listitem-name">Contacts</div>
+                  </li>
+                </ul>
+
+                <ul className="mob-list-extra">
+                  <li className="mob-extralistitem">
+                    <div className="mob-extralistitem-name">
+                      Upcoming Meetings
+                    </div>
+                  </li>
+                  <li className="mob-extralistitem">
+                    <div className="mob-extralistitem-name">
+                      Recent Activities
+                    </div>
+                  </li>
                 </ul>
               </div>
             </div>
